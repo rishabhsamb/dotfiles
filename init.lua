a71f28f6d1e0e0915100829ee762677b3ff41263
Rishabh Sambare <rishabhsamb@gmail.com>
2024-10-17 08:41:22 -0400

    changes

--- init.lua
+++ init.lua
@@ -215,7 +215,65 @@ require('lazy').setup({
     "peterhoeg/vim-qml",
     event = "BufRead",
     ft = { "qml" },
-  }
+  },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  {
+    'prettier/vim-prettier',
+    run = 'npm install',
+    ft = { 'javascript', 'typescript', 'css', 'json', 'markdown' }, -- Add the filetypes you want Prettier for
+    cmd = { 'Prettier', 'PrettierAsync' },  -- Adds the commands to run Prettier manually
+    config = function()
+      -- Optional: format on save
+      vim.cmd([[
+        autocmd BufWritePre *.js,*.ts,*.css,*.json,*.md Prettier
+        ]])
+    end,
+  },
 }, {})
 
 -- [[ Setting options ]]
